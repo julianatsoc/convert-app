c7e105440ebc86ce7e3aaa8c3557a48f9593b01e
Juliana <julianatsoc28@gmail.com>
2025-02-14 21:25:25 -0300

    feat: seleciona os values submetidos e recupera a currencyenviada no form

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
+const USD = 5.70;
+const EUR = 5.98;
+const GBP = 7.17;
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+
+
+
+amount.addEventListener("input", () => {
+    hasCharactersRegex = /\D+/g;
+    amount.value = amount.value.replace(hasCharactersRegex, "");
+})
+
+form.onsubmit = (e) =>{
+    e.preventDefault();
+    switch (currency.value) {
+        case "USD":
+            convertCurrency(amount.value, USD, "US$");
+            break;
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€");
+            break;
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£");
+            break;
+    }
+};
